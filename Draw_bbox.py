8610630870a373a4ff371229e717ed87a278c00d
S16206117 <1458011800@qq.com>
2020-10-19 13:04:10 +0800

    Create Draw_bbox.py

---
+++ Draw_bbox.py
@@ -0,0 +1,23 @@
+from PIL import Image,ImageDraw
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2 as cv
+
+image_path = 'D:/迅雷下载/Aerial_step1_datasets/test/images/1.tif'
+# image_path = 'D:/迅雷下载/Aerial_step1_datasets/train/images/1.tif'
+image = Image.open(image_path)
+#创建一个可以在给定图像上绘图的对象
+draw = ImageDraw.Draw(image)
+
+#draw.polygon([(902,1217),(1288,1215),(1288,1269),(903,1271)], outline=(255,0,0))
+#坐标参数依次是左上角、右上角、右下角、左下角，outline里面是RGB参数：红、绿、蓝
+# draw.polygon([(451,95),(153,39),(749,95),(451,451)], outline=(255,0,0))
+# draw.rectangle([280, 112, 293, 462], outline=(255,0,0))
+# draw.rectangle([634,114,106,30], outline=(255,0,0))
+# draw.rectangle([457,93,140,42], outline=(255,0,0))
+# draw.polygon([789, 300, 183, 372, 1395, 300, 789, 372], outline=(255,0,0))
+draw.polygon([891, 132, 889, 156, 787, 149, 789, 125], outline=(255,0,0))
+draw.polygon([737, 121, 735, 146, 632, 138, 634, 113], outline=(255,0,0))
+draw.polygon([891, 132, 889, 156, 787, 149, 789, 125], outline=(255,0,0))
+draw.polygon([600, 104, 597, 136, 508, 128, 510, 96], outline=(255,0,0))
+image.show()
