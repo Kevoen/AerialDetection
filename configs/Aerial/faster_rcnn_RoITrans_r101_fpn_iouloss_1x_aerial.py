166c510e8b8151cd266f735dfb5999d67652cf13
S16206117 <1458011800@qq.com>
2020-10-28 19:25:12 +0800

    Update faster_rcnn_RoITrans_r101_fpn_iouloss_1x_aerial.py

--- configs/Aerial/faster_rcnn_RoITrans_r101_fpn_iouloss_1x_aerial.py
+++ configs/Aerial/faster_rcnn_RoITrans_r101_fpn_iouloss_1x_aerial.py
@@ -147,7 +147,7 @@ data_root = '../hk/Aerial_step1_datasets/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 data = dict(
-    imgs_per_gpu=1,
+    imgs_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
